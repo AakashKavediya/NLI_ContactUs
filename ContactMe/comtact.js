bf4efa221bb244651b400c0e2c184b9f8c9d3261
Aakash Kavediya <kavediyaaakash@gmail.com>
2025-03-10 16:34:26 +0530

    Removal of action button

--- ContactMe/comtact.js
+++ ContactMe/comtact.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDGnA2Ud_R1686bBnirwF-IEnTTOIO__EM",
-    authDomain: "contactform-c8875.firebaseapp.com",
-    databaseURL: "https://contactform-c8875-default-rtdb.firebaseio.com",
-    projectId: "contactform-c8875",
-    storageBucket: "contactform-c8875.firebasestorage.app",
-    messagingSenderId: "741645275761",
-    appId: "1:741645275761:web:701a976e597574599d41d0"
-  };
-  
+  apiKey: "AIzaSyDGnA2Ud_R1686bBnirwF-IEnTTOIO__EM",
+  authDomain: "contactform-c8875.firebaseapp.com",
+  databaseURL: "https://contactform-c8875-default-rtdb.firebaseio.com",
+  projectId: "contactform-c8875",
+  storageBucket: "contactform-c8875.firebasestorage.app",
+  messagingSenderId: "741645275761",
+  appId: "1:741645275761:web:701a976e597574599d41d0"
+};
+
 firebase.initializeApp(firebaseConfig);
 
 const contactFOrmdb = firebase.database().ref("contactForm");
@@ -15,36 +15,54 @@ const contactFOrmdb = firebase.database().ref("contactForm");
 document.getElementById("contactForm").addEventListener("submit", submitForm);
 
 function submitForm(e) {
-    e.preventDefault();
-    const firstName = getElementVal("firstName");
-    const lastName = getElementVal("lastName");
-    const email = getElementVal("email");
-    const contact = getElementVal("contact");
-    const year = getElementVal("year");
-    const domain = getElementVal("domain");
-    const interest = getElementVal("interest");
-    const question = getElementVal("question");
-    saveMessage(firstName, lastName, email, contact, year, domain, interest, question);
-    document.getElementById("contactForm").reset();
-    window.location.href = "/ThankyouPage/th.html";
-    
-    // console.log(name, email, subject, message);
+  e.preventDefault();
+  const firstName = getElementVal("firstName");
+  const lastName = getElementVal("lastName");
+  const email = getElementVal("email");
+  const contact = getElementVal("contact");
+  const year = getElementVal("year");
+  const domain = getElementVal("domain");
+  const interest = getElementVal("interest");
+  const question = getElementVal("question");
+  saveMessage(
+    firstName,
+    lastName,
+    email,
+    contact,
+    year,
+    domain,
+    interest,
+    question
+  );
+  document.getElementById("contactForm").reset();
+  //   window.location.href = "/th.html";
+
+  // console.log(name, email, subject, message);
 }
 
-const saveMessage  = (firstName, lastName, email, contact, year, domain, interest, question) => {
-    const newContactFormDB = contactFOrmdb.push();
-    newContactFormDB.set({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        contact: contact,
-        year: year,
-        domain: domain,
-        interest: interest,
-        question: question,
-    });
-}
+const saveMessage = (
+  firstName,
+  lastName,
+  email,
+  contact,
+  year,
+  domain,
+  interest,
+  question
+) => {
+  const newContactFormDB = contactFOrmdb.push();
+  newContactFormDB.set({
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    contact: contact,
+    year: year,
+    domain: domain,
+    interest: interest,
+    question: question
+  });
+};
 
 const getElementVal = (id) => {
-    return document.getElementById(id).value;
-}
+  return document.getElementById(id).value;
+};
